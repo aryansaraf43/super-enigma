e780bd4d18230d71869ac08794cdb13a9f484019
aryansaraf43 <sarafneha48@gmail.com>
2025-09-16 14:50:15 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(__dirname));
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user', username => {
+        users[socket.id] = username;
+        socket.broadcast.emit('chat-message', { username: 'System', message: `${username} joined the chat` });
+    });
+
+    socket.on('send-chat-message', message => {
+        const username = users[socket.id];
+        io.emit('chat-message', { username, message });
+    });
+
+    socket.on('disconnect', () => {
+        const username = users[socket.id];
+        socket.broadcast.emit('chat-message', { username: 'System', message: `${username} left the chat` });
+        delete users[socket.id];
+    });
+});
+
+http.listen(3000, () => console.log('Server running on http://localhost:3000'));
